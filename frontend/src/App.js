adf2b1d6853dd8eacf6566f99152f8fdb1155225
Janthathor <Jeffayers06@gmail.com>
2020-10-09 13:45:13 -0400

    Main Body

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,17 @@ import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
+import HomeScreens from './screens/HomeScreens'
 
 const App = () => {
 	return (
 		<>
 			<Header />
-			<Container>
-				<main>
-					<h1>Welcome to ProShop!</h1>
-				</main>
-			</Container>
+			<main className='py-3'>
+				<Container>
+					<HomeScreens></HomeScreens>
+				</Container>
+			</main>
 			<Footer />
 		</>
 	)
